a5a42815d5745839bdc41ac81e48123a9e6e843a
Sonick Mumba <kingellie.mumba@gmail.com>
2026-03-25 21:53:49 +0200

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const authController = require('../controllers/authController');
+const { authenticate } = require('../middleware/auth');
 
 const router = express.Router();
 
 router.post('/signup', authController.register);
 router.post('/login', authController.login);
 router.post('/refresh-token', authController.refreshToken);
-router.post('/logout', authController.logout);
+router.post('/logout', authenticate, authController.logout);
 
 module.exports = router;
