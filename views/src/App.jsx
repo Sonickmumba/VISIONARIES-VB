2c1d3185f6ecf15b1b5f92c8ee55e39812825078
Sonick Mumba <kingellie.mumba@gmail.com>
2026-03-25 21:18:19 +0200

    Update App.jsx

--- views/src/App.jsx
+++ views/src/App.jsx
@@ -1,28 +1,29 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Navbar from './components/Navbar'
+import Layout from './components/Layout'
 import Dashboard from './pages/Dashboard'
 import Groups from './pages/Groups'
 import Cycles from './pages/Cycles'
 import Loans from './pages/Loans'
 import Savings from './pages/Savings'
-import Login from './pages/Login'
-import Register from './pages/Register'
+import { Login } from './pages/Login'
+import { Signup } from './pages/Signup'
 import './App.css'
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Navbar />
         <main className="main-content">
           <Routes>
             <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/" element={<Dashboard />} />
-            <Route path="/groups" element={<Groups />} />
-            <Route path="/cycles" element={<Cycles />} />
-            <Route path="/loans" element={<Loans />} />
-            <Route path="/savings" element={<Savings />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route element={<Layout />}>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/groups" element={<Groups />} />
+              <Route path="/cycles" element={<Cycles />} />
+              <Route path="/loans" element={<Loans />} />
+              <Route path="/savings" element={<Savings />} />
+            </Route>
           </Routes>
         </main>
       </div>
